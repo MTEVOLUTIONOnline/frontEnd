3dfa239f1f82c4983224152d4acc3164139605d3
MTEVOLUTIONOnline <mtevolution0@gmail.com>
2024-10-12 12:42:47 +0100

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
-// Configuração para redirecionar todas as rotas para o index.html, útil para SPAs
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -10,18 +9,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    outDir: "dist",
-  },
-  server: {
-    port: 3000,
-    open: true,
-  },
-  // Redireciona todas as requisições para o index.html para lidar com rotas do SPA
-  esbuild: {
-    jsxInject: `import React from 'react'`
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom'],
-  }
-});
+})
